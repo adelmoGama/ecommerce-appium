32a5938e6f839742e3a0307f853f6a6d43aad6c3
unknown <gama.a.jose@gmail.com>
2024-07-19 14:07:51 -0300

    Method just to verify the connection with the emulator - WIP

---
+++ ecommerceAppium/src/test/java/ecommerceAppium/AppiumTest.java
@@ -0,0 +1,11 @@
+package ecommerceAppium;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest extends AppiumConectionConfig{
+
+    @Test
+    public void AppiumConnectionTest() throws InterruptedException {
+        Thread.sleep(5000);
+    }
+}
