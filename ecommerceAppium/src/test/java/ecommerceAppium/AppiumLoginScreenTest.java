56001176d9395449de5b49b25fe791d2cbd8cea5
unknown <gama.a.jose@gmail.com>
2024-07-25 11:51:04 -0300

    Using new annotations to skip some knew invalid tests - WIP

--- ecommerceAppium/src/test/java/ecommerceAppium/AppiumLoginScreenTest.java
+++ ecommerceAppium/src/test/java/ecommerceAppium/AppiumLoginScreenTest.java
@@ -49,27 +49,51 @@ public class AppiumLoginScreenTest extends AppiumConectionConfig {
         Assert.assertEquals(toastMessage, "Please enter your name");
     }
 
-    // THE APPLICATION DOESN'T SET MINIMUM AND MAXIMUM VALUES TO THE NAME FIELD
-//    @Test
-//    public void LoginAttemptFillingNameFieldWith1CaracterTest() {
-//        driver.findElement(By.id("com.androidsample.generalstore:id/spinnerCountry")).click();
-//        driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Brazil\"));")).click();
-//        driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("v");
-//
-//        driver.hideKeyboard();
-//
-//        driver.findElement(By.xpath("//android.widget.RadioButton[@text='Female']")).click();
-//
-//        String clientName = driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).getText();
-//        String countyName = driver.findElement(By.id("android:id/text1")).getText();
-//
-//        Assert.assertEquals(clientName, "v");
-//        Assert.assertEquals(countyName, "Brazil");
-//
-//        driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
-//
-//        String toastMessage = driver.findElement(By.xpath("(//android.widget.Toast)[1]")).getAttribute("name");
-//
-//        Assert.assertEquals(toastMessage, "Please enter your name");
-//    }
+    // THE APPLICATION DIDN'T SET A MINIMUM VALUE TO THE NAME FIELD
+    @Test(enabled = false)
+    public void LoginAttemptFillingNameFieldWith1CharacterTest() {
+        driver.findElement(By.id("com.androidsample.generalstore:id/spinnerCountry")).click();
+        driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Brazil\"));")).click();
+        driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("v");
+
+        driver.hideKeyboard();
+
+        driver.findElement(By.xpath("//android.widget.RadioButton[@text='Female']")).click();
+
+        String clientName = driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).getText();
+        String countyName = driver.findElement(By.id("android:id/text1")).getText();
+
+        Assert.assertEquals(clientName, "v");
+        Assert.assertEquals(countyName, "Brazil");
+
+        driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+
+        String toastMessage = driver.findElement(By.xpath("(//android.widget.Toast)[1]")).getAttribute("name");
+
+        Assert.assertEquals(toastMessage, "Please enter your name");
+    }
+
+    // THE APPLICATION DIDN'T SET A MAXIMUM VALUE TO THE NAME FIELD
+    @Test(enabled = false)
+    public void LoginAttemptFillingNameFieldWith70CharactersTest() {
+        driver.findElement(By.id("com.androidsample.generalstore:id/spinnerCountry")).click();
+        driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Brazil\"));")).click();
+        driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv");
+
+        driver.hideKeyboard();
+
+        driver.findElement(By.xpath("//android.widget.RadioButton[@text='Female']")).click();
+
+        String clientName = driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).getText();
+        String countyName = driver.findElement(By.id("android:id/text1")).getText();
+
+        Assert.assertEquals(clientName, "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv");
+        Assert.assertEquals(countyName, "Brazil");
+
+        driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+
+        String toastMessage = driver.findElement(By.xpath("(//android.widget.Toast)[1]")).getAttribute("name");
+
+        Assert.assertEquals(toastMessage, "Number of characters bigger than the maximum value - 50");
+    }
 }
